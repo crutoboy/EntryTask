8de9479eb086507b81b90c1d54e65c9016d65f6d
crutoboy <vanyalex0192837465@gmail.com>
2024-05-11 01:29:59 +0300

    task 2 completed

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,31 +1,51 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<!-- <script lang="ts">
+  // import svelteLogo from './assets/svelte.svg'
+  // import viteLogo from '/vite.svg'
+  // import Counter from './lib/Counter.svelte'
+</script> -->
+
+<script>
+  import { rates, translate } from '/src/currency.js' 
+
+  let currency1 = 'USD'
+  let currency2 = 'RUB'
+  let valueMoney1 = 1
+  let valueMoney2 = rates['RUB']
+
+  function onChageFirstCurrency(){
+    valueMoney2 = valueMoney1 / rates[currency1] * rates[currency2]
+  }
+
+  function onChageSecondCurrency(){
+    valueMoney1 = valueMoney2 / rates[currency2] * rates[currency1]
+  }
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="container">
+    <select name="" id="" bind:value={currency1} on:change={onChageFirstCurrency}>
+      {#each Object.keys(rates) as currency}
+        <option value={currency}>
+          {`${translate(currency)} (${currency})`}
+        </option>
+      {/each}
+    </select>
+
+    <select name="" id="" bind:value={currency2} on:change={onChageSecondCurrency}>
+      {#each Object.keys(rates) as currency}
+        <option value={currency}>
+          {`${translate(currency)} (${currency})`}
+        </option>
+      {/each}
+    </select> 
   </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
+  <div class="container">
+    <input type="number" name="" id="" class="inp" bind:value={valueMoney1} on:change={onChageFirstCurrency}>
+    <input type="number" name="" id="" class="inp" bind:value={valueMoney2} on:change={onChageSecondCurrency}>
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
